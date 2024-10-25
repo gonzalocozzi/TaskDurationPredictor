84f499cea0a5e496a7bf3e2e15e351ddb65d6681
Gonzalo Alejandro Cozzi <gcozzi@outlook.com.ar>
2024-10-24 21:59:49 -0300

    Se agrega una versión del algoritmo que contempla la duración de la ejecución actual para considerar la duración que el proceso va teniendo en tiempo real

--- TaskDurationPredictor/TaskManager.cs
+++ TaskDurationPredictor/TaskManager.cs
@@ -46,8 +46,8 @@ namespace TaskDurationPredictor
                     break;
                 }
 
-                await Task.Delay(500); // Simula un segundo de trabajo
-                elapsed++;
+                await Task.Delay(500); // Simula medio segundo de trabajo
+                elapsed += 0.5;
                 progress = elapsed / actualDuration * 100;
                 if (progress > 100) progress = 100;
 
@@ -59,7 +59,61 @@ namespace TaskDurationPredictor
             if (progress == 100)
             {
                 actualDurationMessage.Invoke(actualDuration);
-                _repository.AddOrUpdatetaskHistory(taskName, actualDuration);
+                _repository.AddOrUpdateTaskHistory(taskName, actualDuration);
+            }
+        }
+
+        public async Task SimulateTaskAsyncV2(string taskName,
+                                            Action<double, double?> onProgressUpdated,
+                                            Action<double> averageDurationMessage,
+                                            Action<double> actualDurationMessage,
+                                            CancellationToken cancellationToken)
+        {
+            double actualDuration;
+            double averageDuration = 0;
+            bool usePrediction = _repository.HasTaskHistory(taskName);
+
+            if (usePrediction)
+            {
+                averageDuration = _repository.GetAverageDuration(taskName);
+                double randomFactor = random.NextDouble() * 1.5 + 0.1;
+                actualDuration = averageDuration * randomFactor;
+                averageDurationMessage.Invoke(averageDuration);
+            }
+            else
+            {
+                actualDuration = random.Next(5, 30); // Duración aleatoria sin predicción
+            }
+
+            double progress = 0;
+            double elapsed = 0;
+
+            while (progress < 100)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Simulación cancelada.");
+                    break;
+                }
+
+                var randomElapsedTime = random.Next(10, 100);
+                await Task.Delay(randomElapsedTime); // Simula medio segundo de trabajo
+                elapsed += randomElapsedTime / 1000d;
+                Console.WriteLine(elapsed);
+                progress++;
+
+                var elapsedTimePrediction = 100 * elapsed / progress;
+                var currentAverageDuration = (averageDuration + elapsedTimePrediction) / 2;
+
+                double? estimatedRemaining = usePrediction ? currentAverageDuration - elapsed : null;
+                if (estimatedRemaining.HasValue && estimatedRemaining < 0) estimatedRemaining = 0;
+                onProgressUpdated?.Invoke(progress, estimatedRemaining); // Llama al callback
+            }
+
+            if (progress == 100)
+            {
+                actualDurationMessage.Invoke(elapsed);
+                _repository.AddOrUpdateTaskHistory(taskName, elapsed);
             }
         }
     }
